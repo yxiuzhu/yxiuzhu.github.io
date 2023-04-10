6ebfba2edfe24ddbb8bd0c5fb7e492e339da81e2
y_xiuzhu <y_xiuzhu@163.com>
2023-04-11 00:52:42 +0800

    feat: 初始化项目及首页

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,11 @@
+// .vitepress/theme/index.js
+import Layout from './Layout.vue'
+
+import DefaultTheme from 'vitepress/theme'
+
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    // app.component('Layout', Layout)
+  }
+}
