944324da7cf96df705777a73ea0d2d2756b17076
y_xiuzhu <y_xiuzhu@163.com>
2023-04-19 00:42:20 +0800

    chore: 部署个人网站

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'HamWeb',
   description: 'Just playing web.',
+  base: '/ham-blog/',
   themeConfig: {
     siteTitle: 'Ham Web', // 重置首页标题
     logo: 'images/my-logo-title.svg',
